45983c8dde72a53827c372815729a99af1cabd90
sagor1155 <sagor.eee.cuet@gmail.com>
2020-01-29 21:36:30 +0600

    my first java program

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+
+
+public class MyFirstJavaProgram 
+{
+    public static void main(String []args) 
+    {
+        for(int i=0; i<3; i++){
+            System.out.println("Hello World, i'm learning java"); 
+        }
+    }
+
+}
